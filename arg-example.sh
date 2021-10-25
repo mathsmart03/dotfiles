43c0c0efaa42a5e89f1779b3d5395dbb501b966c
Rejyr <jerrylwang123@gmail.com>
2021-10-25 06:45:41 -0400

    Split configs into groups by window manager. Added nonfunctional command line args to scripts.

--- arg-example.sh
+++ arg-example.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
-############################
-# Moves files/directories in config to dir and creates symlinks
-############################
-
-############################
-# Default directories
-config=~/.config
-dir=~/dotfiles/.config
-olddir=~/dotfiles/.config-old
-############################
-
-# get args
 
 ARGS=$(getopt -a --options c:d:o --long "config:,directory:,old-directory:" -- "$@")
 
@@ -33,6 +21,3 @@ while true; do
       break;;
   esac
 done
-
-exec ./move-dotfiles.sh -c $config -d $dir -o $olddir
-exec ./make-symlinks.sh -c $config -d $dir -o $olddir
--- make-symlinks.sh
+++ make-symlinks.sh
@@ -6,10 +6,33 @@
 ########## Variables
 
 config=~/.config			  # absolute config directory
-dir=~/dotfiles/.config                    # absolute dotfiles directory
-olddir=~/dotfiles/.config-old		  # absolute old dotfiles backup directory
+dir=~/dotfiles/configs/base/.config                    # absolute dotfiles directory
+olddir=~/dotfiles/configs/base/.config-old		  # absolute old dotfiles backup directory
 
 ##########
+
+# get args
+
+ARGS=$(getopt -a --options c:d:o --long "config:,directory:,old-directory:" -- "$@")
+
+eval set -- "$ARGS"
+
+while true; do
+  case "$1" in
+    -c|--config)
+      config="$2"
+      shift 2;;
+    -d|--directory)
+      dir="$2"
+      shift 2;;
+    -o|--old-directory)
+      olddir="$2"
+      shift 2;;
+    --)
+      break;;
+  esac
+done
+
 
 # create dotfiles backup directory
 if [ ! -d $olddir ]
