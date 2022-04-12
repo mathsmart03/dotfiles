5171aa3d5d9a293ad76578d7b24bef5568ded775
Rejyr <jerrylwang123@gmail.com>
2022-04-12 10:28:05 -0400

    update: dotfiles. add: waybar weather. update: background

---
+++ .config/waybar/weather.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# Updates weather forecast
+
+ip r | while read Def _ IP _; do
+	case $Def in
+		default)
+			ping -q -w 1 -c 1 "$IP" >/dev/null || exit
+
+			break ;;
+	esac
+done
+
+geoip=$(curl -s https://freegeoip.app/csv/)
+country_code=$(echo $geoip | cut -d, -f 2)
+location=$(echo $geoip | cut -d, -f 6)
+ansiweather -l $location,$country_code -u imperial -s true -f 1 -a false | cut -d' ' -f2,7,8-
